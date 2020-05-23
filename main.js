d4f472de8fdac77c6ce9038ac049f82b7ae624ae
surajprasad13 <surajvishwakarma830@gmail.com>
2020-05-23 19:29:15 +0530

    commit

---
+++ main.js
@@ -0,0 +1,13 @@
+fetch("https://restcountries-v1.p.rapidapi.com/all", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+    "x-rapidapi-key": "137332e08cmsh83a73a314e0a5b2p13aa8djsn84ba447f737f",
+  },
+})
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
